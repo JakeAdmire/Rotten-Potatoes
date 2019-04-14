e9905d27d38bda87c44c57df48e7fbc7d7f0260f
Jake <jakeadmire1@gmail.com>
2019-04-14 11:18:54 -0600

    Add reducers to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,11 @@
 import { combineReducers } from 'redux';
 
+import { restaurantsReducer } from './restaurantsReducer';
+import { locationReducer } from './locationReducer';
+import { redirectReducer } from './redirectReducer';
+
 export const rootReducer = combineReducers({
+  restaurants: restaurantsReducer,
+  location: locationReducer,
+  redirect: redirectReducer
 })
