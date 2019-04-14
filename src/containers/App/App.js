8826e66d4d34cf286833348793306ea111effbbe
Jake <jakeadmire1@gmail.com>
2019-04-14 10:37:12 -0600

    Build fetchRestaurants method to connect with API

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.fetchRestaurants();
+  }
+
+  async fetchRestaurants() {
+    const url = 'https://data.colorado.gov/resource/d5e8-gubm.json';
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      // this.gatherRestaurantNames(data);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Rotten Potatoes</h1>
       </div>
     );
   }
