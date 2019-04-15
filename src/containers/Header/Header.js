db67d927f455cc839bed5143a32d063c137b591e
Jake <jakeadmire1@gmail.com>
2019-04-15 12:50:57 -0600

    Build and style Header container

---
+++ src/containers/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { setRedirect } from '../../actions';
+
+const Header = (props) => {
+
+  const redirect = () => {
+    props.setRedirect('');
+  }
+  // console.log(props.location.name);
+  return (
+    <div className="Header">
+      <div className="title">
+        <h1>Rotten Potatoes</h1>
+      </div>
+      <div className="nav">
+        <p>Showing Results for:</p>
+        <p>{ props.location && props.location.name }</p>
+        <button onClick={redirect}>change location..</button>
+      </div>
+    </div>
+  )
+}
+
+export const mapStateToProps = (state) => ({
+  location: state.location
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  setRedirect: (redirect) => dispatch(setRedirect(redirect)), 
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
