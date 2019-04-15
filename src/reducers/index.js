e6e5e658b2a3ce14d402e188776ddb8f6fad65db
Jake <jakeadmire1@gmail.com>
2019-04-15 12:50:18 -0600

    Build loadingReducer and add it to the rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import { restaurantsReducer } from './restaurantsReducer';
 import { locationReducer } from './locationReducer';
 import { redirectReducer } from './redirectReducer';
+import { loadingReducer } from './loadingReducer';
 
 export const rootReducer = combineReducers({
   restaurants: restaurantsReducer,
   location: locationReducer,
-  redirect: redirectReducer
+  redirect: redirectReducer,
+  loading: loadingReducer
 })
