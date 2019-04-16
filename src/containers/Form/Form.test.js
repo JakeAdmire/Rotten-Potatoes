d32fe76a48002522d9cd3318660c7409b3cec717
Jake <jakeadmire1@gmail.com>
2019-04-16 13:49:52 -0600

    Test Form.js default state and state changes

--- src/containers/Form/Form.test.js
+++ src/containers/Form/Form.test.js
@@ -7,15 +7,64 @@ import { setRestaurants, setLocation, setRedirect, isLoading } from '../../actio
 describe('Form', () => {
 
   let wrapper;
+  let mockHandleClick = jest.fn();
+  let mockGetLocation = jest.fn();
+  let mockSetLocation = jest.fn();
+  let mockSetRedirect = jest.fn();
+  let mockHandleEnter = jest.fn();
 
   beforeEach(() => {
     wrapper = shallow(
-      <Form />
+      <Form handleClick={mockHandleClick}
+            getLocation={mockGetLocation}
+            setLocation={mockSetLocation}
+            setRedirect={mockSetRedirect}
+            handleEnter={mockHandleEnter} />
     )
   })
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should have default state', () => {
+    let mockState = { value: '' };
+    expect(wrapper.state()).toEqual(mockState);
+  })
+
+  it.skip('should call handleClick on click', () => {
+    let handleClick = jest.fn();
+    wrapper = shallow( <Form  handleClick={handleClick} />);
+    wrapper.update();
+    wrapper.find('#test').simulate('click');
+    expect(handleClick).toHaveBeenCalled();
+  })
+
+  it.skip('should call handleEnter if key is pressed', () => {
+    const mockEvent = { key: 'Enter' };
+    const instance = wrapper.instance();
+    let mockSpy = jest.spyOn(instance, 'handleEnter');
+    wrapper.setState({ value: 'test'});
+    wrapper.find('div').simulate('keypress', mockEvent);
+    expect(mockSpy).toHaveBeenCalled();
+  })
+
+  it('should dispatch setLocation when gatherLocationInfo is called', () => {
+    const mockResult = { 
+      formatted_address: 'Denver',
+      geometry: { location: {} } 
+    };
+    wrapper.instance().gatherLocationInfo(mockResult);
+    expect(mockSetLocation).toHaveBeenCalled();
+  })
+
+  it('should dispatch setRedirect when gatherLocationInfo is called', () => {
+    const mockResult = { 
+      formatted_address: 'Denver',
+      geometry: { location: {} } 
+    };
+    wrapper.instance().gatherLocationInfo(mockResult);
+    expect(mockSetRedirect).toHaveBeenCalled();
   })
 
   describe('mapStateToProps', () => {
