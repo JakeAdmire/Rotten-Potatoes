542c515d8eb376d5b41e7c4cf8d11eb8234bf27a
Jake <jakeadmire1@gmail.com>
2019-04-16 13:49:20 -0600

    Test CardContainer default state and state changes

--- src/containers/CardContainer/CardContainer.test.js
+++ src/containers/CardContainer/CardContainer.test.js
@@ -7,15 +7,50 @@ import { isLoading } from '../../actions';
 describe('CardContainer', () => {
 
   let wrapper;
+  let mockIsLoading = jest.fn();
 
   beforeEach(() => {
     wrapper = shallow(
-      <CardContainer />
+      <CardContainer  isLoading={mockIsLoading}/>
     )
   })
 
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
+  })
+
+  it('should have default state', () => {
+    let mockState = { cards: [] };
+    expect(wrapper.state()).toEqual(mockState);
+  })
+
+  it.skip('should call sortRestaurants when gatherRestaurants is invoked', () => {
+    let mockRestaurants = [
+      { distance: 13.2131231 },
+      { distance: 15.2341134 } 
+    ];
+    let instance = wrapper.instance();
+    jest.spyOn(instance, 'sortRestaurants');
+    wrapper.instance().gatherRestaurants(mockRestaurants);
+    expect(instance.sortRestaurants).toBeCalled();
+  })
+
+  it('should set state when sortByDistance is called', () => {
+    let mockRestaurants = [
+      { distance: 13.2131231 },
+      { distance: 15.2341134 } 
+    ];
+    wrapper.instance().sortByDistance(mockRestaurants);
+    expect(wrapper.state('cards')).toEqual(mockRestaurants);
+  })
+
+  it('should dispatch isLoading when sortByDistance is called', () => {
+    let mockRestaurants = [
+      { distance: 13.2131231 },
+      { distance: 15.2341134 } 
+    ];
+    wrapper.instance().sortByDistance(mockRestaurants);
+    expect(mockIsLoading).toHaveBeenCalled();
   })
 
   describe('mapStateToProps', () => {
