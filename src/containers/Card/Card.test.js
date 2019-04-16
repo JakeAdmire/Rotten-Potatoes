81b2704f58ee3573d5c9284d136eb9b9e5ca6678
Jake <jakeadmire1@gmail.com>
2019-04-16 13:48:39 -0600

    Test Card.js method snapshots

--- src/containers/Card/Card.test.js
+++ src/containers/Card/Card.test.js
@@ -6,14 +6,38 @@ import { Card } from './Card';
 describe('Card', () => {
 
   let wrapper;
+  const mockProps = {
+    facilityname: 'Taco Bell',
+    address: '123 Street St. Denver, CO',
+    inspectionscore: 25,
+    violationtype: 'critical'
+  }
 
   beforeEach(() => {
     wrapper = shallow (
-      <Card />
+      <Card {...mockProps} />
     )
   })
 
   it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('buildTitle should return null if props is empty', () => {
+    wrapper = shallow( <Card /> )
+    wrapper.instance().buildTitle();
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('buildAddress should return null if props is empty', () => {
+    wrapper = shallow( <Card /> )
+    wrapper.instance().buildAddress();
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('buildDistance should return null if props is empty', () => {
+    wrapper = shallow( <Card /> )
+    wrapper.instance().buildDistance();
     expect(wrapper).toMatchSnapshot();
   })
   
