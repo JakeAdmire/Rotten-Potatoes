e230418065d88892b28a650a5d0a0fdcddd14ab4
Jake <jakeadmire1@gmail.com>
2019-04-16 19:17:04 -0600

    Implement shortid package into restaurantsReducer.js

--- src/reducers/restaurantsReducer.js
+++ src/reducers/restaurantsReducer.js
@@ -1,8 +1,13 @@
+// const shortid = require('shortid');
+import shortid from 'shortid';
+
 export const restaurantsReducer = (state = [], action) => {
   switch(action.type) {
 
     case('SET_RESTAURANTS') :
-      return action.restaurants;
+      return action.restaurants.map(restaurant => {
+        return { name: restaurant, id: shortid.generate() }
+      });
 
     default :
       return state;
