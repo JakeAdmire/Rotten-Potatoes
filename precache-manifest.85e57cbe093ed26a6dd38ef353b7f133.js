7a934fde22721f10cb6df46b0edc35ec35efe022
Jake <jakeadmire1@gmail.com>
2019-12-24 14:01:44 -0700

    Updates

---
+++ precache-manifest.85e57cbe093ed26a6dd38ef353b7f133.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "07174e6ae8097b27d82b",
+    "url": "/Rotten-Potatoes/static/css/main.4ab41baa.chunk.css"
+  },
+  {
+    "revision": "07174e6ae8097b27d82b",
+    "url": "/Rotten-Potatoes/static/js/main.605bf662.chunk.js"
+  },
+  {
+    "revision": "8ab2f61e0bdeb5df0443",
+    "url": "/Rotten-Potatoes/static/js/runtime~main.c748ae5e.js"
+  },
+  {
+    "revision": "7b01f76fc82320ec360a",
+    "url": "/Rotten-Potatoes/static/js/2.c3d50fd4.chunk.js"
+  },
+  {
+    "revision": "dcbd441caf526e7a0fb56e7e4921ef59",
+    "url": "/Rotten-Potatoes/static/media/91.dcbd441c.svg"
+  },
+  {
+    "revision": "a9a75cddb6b73882512f3fa7bae5b91d",
+    "url": "/Rotten-Potatoes/static/media/background.a9a75cdd.jpg"
+  },
+  {
+    "revision": "1d532f15d582b6c95e659ee0d2c1676b",
+    "url": "/Rotten-Potatoes/index.html"
+  }
+];
